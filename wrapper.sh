922041da4174bdf385c360f9c0e69ae97f87f869
Akshay Jain <akshaykrjain@users.noreply.github.com>
2019-03-21 20:29:12 +0100

    Create wrapper.sh

---
+++ wrapper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#!Author AkshayJain
+
+sudo apt-get update
+
+echo "######Installing Virtual Box"
+sudo apt-get -y install virtualbox
+
+echo "######Installing Vagrant"
+sudo apt-get -y install vagrant
+
+
+echo "######Get the stuff"
+git clone https://github.com/akshaykrjain/vagrant_go.git
+
+echo "######Installing and Starting"
+cd vagrant-go; vagrant up
+
+echo "####Done.. Please verify by using"
+echo "curl <ip of vagrant box>:8080/hello/by/go/from/Vagrant"
+
+
